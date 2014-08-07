20a1b66e657e2009c402737a564757aa5e130936
Kris Arnold <karnold@dma.org>
2014-08-07 17:55:02 -0500

    add index to user_id column

---
+++ updates/add_indexes.php
@@ -0,0 +1,26 @@
+<?php namespace DMA\FriendsRE\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class AddIndexesTable extends Migration
+{
+
+    public function up()
+    {   
+        Schema::table('dma_friendsre_razors_edges', function($table)
+        {   
+            $table->index('user_id');
+        }); 
+
+    }   
+
+    public function down()
+    {   
+        Schema::table('dma_friendsre_razors_edges', function($table)
+        {   
+            $table->dropIndex('user_id');
+        }); 
+    }   
+
+}
